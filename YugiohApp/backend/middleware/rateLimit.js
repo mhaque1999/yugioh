652eb6f88e485f72dd4fce1bc2d5eafe9013fbb1
mhaque1999 <150069756+mhaque1999@users.noreply.github.com>
2024-08-08 14:15:32 -0400

    Delete YugiohApp directory

--- YugiohApp/backend/middleware/rateLimit.js
+++
@@ -1,9 +0,0 @@
-const rateLimit = require('express-rate-limit');
-
-const apiLimiter = rateLimit({
-    windowMs: 1000, 
-    max: 20, 
-    message: 'Rate limit exceeded. Please try again later.'
-});
-
-module.exports = apiLimiter;
